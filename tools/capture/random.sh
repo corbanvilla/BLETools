fdc63f2df7af2e0d99ff5a9b1fb5f9fdca79a14c
animcogn <animcogn@mousetrap3.1>
2019-02-22 00:40:05 -0700

    added extra files

--- tools/capture/random.sh
+++ tools/capture/random.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-
 MACADDR="$(cat macaddr)"
 
-white true; do
+while true; do
 	RAND="$(openssl rand -hex 3)"
-	PARAMS="--adapter=hci1 --device=$MACCADDR --handle=0x000b --value=56"$RAND"fff0aa --char-write-req"
+	PARAMS="--adapter=hci0 --device=$MACADDR --handle=0x000b --value=56"$RAND"fff0aa --char-write-req"
 	gatttool $PARAMS
 	sleep 2
 done
